bb4356cd233d2a9d23efc78be46c7e3d94f7d85f
Damian Padilla <damipad4@gmail.com>
2024-01-20 15:44:12 -0600

    init project

---
+++ damian-24/src/Nav/Nav.jsx
@@ -0,0 +1,17 @@
+import "./Nav.css";
+
+const Nav = () => {
+  return (
+    <>
+      <section className="NavContainer">
+        <ul className="NavItem">
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </section>
+    </>
+  );
+};
+
+export default Nav;
