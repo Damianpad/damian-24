2c2877d17dcbc9319f403e9c47ec9dafff8931b5
Damian Padilla <damipad4@gmail.com>
2024-02-02 15:30:26 -0600

    added background and title component

--- damian-24/src/App.jsx
+++ damian-24/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Nav from "./Nav/Nav";
+import Title from "./components/Title/Title";
 
 function App() {
   return (
-    <>
+    <section className="pageContainer">
       <Nav />
-    </>
+      <Title />
+    </section>
   );
 }
 
---
+++ damian-24/src/components/Title/Title.jsx
@@ -0,0 +1,10 @@
+const Title = () => {
+  return (
+      <section className="titleContainer">
+        <h1>Damian Padilla</h1>
+        <h2>Fullstack Developer</h2>
+      </section>
+  );
+};
+
+export default Title;
